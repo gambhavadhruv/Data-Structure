e309fdbd7042c3657320e3a5b52f060ff3b96d3e
Dhruv Gambhava <108172163+Dhruvv09@users.noreply.github.com>
2022-07-01 21:59:32 +0530

    Update Clock_Angle.java

--- Clock_Angle.java
+++ Clock_Angle.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Clock{
+public class Clock_Angle{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter the hour : ");
